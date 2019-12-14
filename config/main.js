2869b80c913abb5975bdc4581037ac0249468fac
mestanza-93 <pablo93ml@gmail.com>
2019-12-14 18:21:09 +0100

    chore: data from Form to Datatable

--- config/main.js
+++ config/main.js
@@ -4,16 +4,12 @@ const path = require('path')
 
 const db = require('./db')
 global.database = db
-console.log(db.connect().customer);
 
 const { app, BrowserWindow, ipcMain } = electron
 
-// Mantén una referencia global del objeto window, si no lo haces, la ventana 
-// se cerrará automáticamente cuando el objeto JavaScript sea eliminado por el recolector de basura.
 let mainWindow
 
 function createWindow () {
-  // Crea la ventana del navegador.
   mainWindow = new BrowserWindow({
     width: 1800,
     height: 1000,
@@ -22,42 +18,28 @@ function createWindow () {
     }
   })
 
-  // y carga el index.html de la aplicación.
   mainWindow.loadURL(url.format({
     pathname: path.join( __dirname, '..', 'src', 'views', 'index.html' ),
     protocol: 'file:',
     slashes: true
   }))
 
-  // Abre las herramientas de desarrollo (DevTools).
   mainWindow.webContents.openDevTools()
 
-  // Emitido cuando la ventana es cerrada.
   mainWindow.on('closed', () => {
-    // Elimina la referencia al objeto window, normalmente  guardarías las ventanas
-    // en un vector si tu aplicación soporta múltiples ventanas, este es el momento
-    // en el que deberías borrar el elemento correspondiente.
     mainWindow = null
   })
 }
 
-// Este método será llamado cuando Electron haya terminado
-// la inicialización y esté listo para crear ventanas del navegador.
-// Algunas APIs pueden usarse sólo después de que este evento ocurra.
 app.on('ready', createWindow)
 
-// Sal cuando todas las ventanas hayan sido cerradas.
 app.on('window-all-closed', () => {
-  // En macOS es común para las aplicaciones y sus barras de menú
-  // que estén activas hasta que el usuario salga explicitamente con Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // En macOS es común volver a crear una ventana en la aplicación cuando el
-  // icono del dock es clicado y no hay otras ventanas abiertas.
   if (mainWindow === null) {
     createWindow()
   }
--- src/models/customer.js
+++ src/models/customer.js
@@ -2,7 +2,11 @@ const remote = require('electron').remote;
 const db = remote.getGlobal('database');
 const customerDB = db.connect().customer;
 
+const columnsDatatable = [ 'name', 'lastname', 'phone', 'address' ];
 
+/*
+Get data from the form and format it to insert.
+*/
 function getFormData () {
     var data = {};
     document.querySelectorAll("input").forEach(ele => data[ele.name] = ele.value || "");
@@ -11,6 +15,10 @@ function getFormData () {
 }
 
 
+/*
+Create a new Customer in database calling
+getFormData() to get info from the form.
+*/
 function insertCustomer () {
     var data = getFormData();
     if (data) {
@@ -27,23 +35,39 @@ function insertCustomer () {
 }
 
 
+/* 
+Get all customers data then call processCustomersData()
+to format the data for Datatables.
+*/ 
 function getAllCustomers () {
     customerDB.find({}, function(err, data) {
         if (err) {
             console.log("ERROR: ", err);
-            return {};
+            return [];
         } else {
             var customers = processCustomersData(data);
-            console.log("Customers: ", data);
+            console.log("Customers: ", customers);
             return customers;
         }
     });
 }
 
+
+/*
+ Get columns to show in Datatables and format this data to 
+ show correctly in Datatables.
+ */
 function processCustomersData (customers) {
-    data = [];
-    customers.forEach(ele => data[ele.name] = ele.value || "");
-    console.log(data);
+    var data = [];
+
+    for (element of customers) {
+        var ele = {};
+        for (column of columnsDatatable){
+            ele[column] = element[column];
+        }
+        data.push(ele);
+    }
+    return data;
 }
 
 
